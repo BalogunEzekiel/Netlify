992494db29efa008435a507829a373ab40a64ba0
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-16 15:09:42 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // Let frontend talk to backend
+
+app.get('/hello', (req, res) => {
+  res.send('Hello, friend!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
